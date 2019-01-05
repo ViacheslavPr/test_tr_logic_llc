68fc2da3c0e3577463ab53e72db03439008b6db8
Viacheslav <bogdanov_slava_90@mail.ru>
2019-01-05 14:36:44 +0300

    heroku setting

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -50,7 +50,7 @@ export const setCurrentUser = decoded => {
 
 // Log user out
 export const logoutUser = () => dispatch => {
-  //Remove token from loacalStorage
+  //Remove token from localStorage
   localStorage.removeItem("jwtToken");
   // Remove auth header for future requests
   setAuthToken(false);
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://slava:slava1@ds247944.mlab.com:47944/test_tr_logic_llc",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
